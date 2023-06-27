be59494e25714de2646b8bd5bad9398ad584cc73
Toteck <53489827+Toteck@users.noreply.github.com>
2023-06-27 00:52:19 +0000

    style: deixando head responsivo

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import logo from 'src/Logos/AC.png'
+import './style.css';
+
+export function Header() {
+  const isMobile = useMediaQuery({ maxWidth: 480 });
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  function toggleMenu() {
+    setMenuOpen(!menuOpen);
+  }
+
+  return (
+    <div>
+      <div className='header'>
+        <div className="logo">
+          <a>
+            <img scr={logo} />
+          </a>
+        </div>
+        {isMobile && (
+          <button onClick={toggleMenu}>Menu</button>
+        )}
+      </div>
+
+      <div id='menu'>
+        <ul className={`sidebar ${menuOpen ? 'active' : ''}`}>
+          <a><li>Início</li></a>
+          <a><li>Burgers</li></a>
+          <a><li>Serviços</li></a>
+          <a><li>Contato</li></a>
+        </ul>
+      </div>
+    </div>
+  )
+
+}
